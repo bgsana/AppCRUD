b2e3091727b2384b33bb311002b57da931b42f40
bgsana <bgs.ana08@gmail.com>
2025-11-17 22:28:54 -0300

    Feito consumo de API

--- App.js
+++ App.js
@@ -1,37 +1,30 @@
+import { useEffect, useState } from 'react'
 import { FlatList, View, StyleSheet } from 'react-native'
-import { Card, Text } from 'react-native-paper'
+import axios from 'axios'
+import { ActivityIndicator, Card, Text } from 'react-native-paper'
 
 export default function App() {
-  const produtos = [
-    {
-      id: 1,
-      nome: 'Casalzinho Snoopy',
-      descricao: 'Amor em forma de amigurumi! Casalzinho Snoopy disponível na AmiguLand — feito à mão pra derreter corações',
-      imagem: 'https://cdn.pixabay.com/photo/2022/04/20/20/59/crochet-bear-7146190_1280.jpg'
-    },
-    {
-      id: 2,
-      nome: 'Chewbacca',
-      descricao: 'Direto de uma galáxia muito, muito distante… Chewbacca chegou em versão chaveirinho amigurumi! Perfeito pra levar a Força geek sempre com você. Encomendas abertas!',
-      imagem: 'https://cdn.pixabay.com/photo/2018/03/22/11/01/amigurumi-3250032_1280.jpg'
-    },
-    {
-      id: 3,
-      nome: 'Lotso',
-      descricao: 'Lotso, direto do Toy Story pra AmiguLand! Fofo demais pra ser vilão. Feito à mão com carinho, encomendas abertas',
-      imagem: 'https://cdn.pixabay.com/photo/2018/03/22/11/01/amigurumi-3250031_1280.jpg'
-    },
-    {
-      id: 4,
-      nome: 'Sulley',
-      descricao: 'Fofura em versão chaveiro!',
-      imagem: 'https://cdn.pixabay.com/photo/2018/07/31/14/10/amigurumi-3575171_1280.jpg'
-    }
-  ]
+  const [produtos, setProdutos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const API_URL = 'https://691bc2a03aaeed735c8e2d67.mockapi.io/produtos/api/v1/produto'
+
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(response => {
+        setProdutos(response.data)
+      })
+      .catch(error => {
+        console.log('Erro ao buscar os produtos', error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  })
 
   const renderItem = ({ item }) => (
     <Card style={styles.card}>
-      <Card.Cover source={{uri: item.imagem}} />
+      <Card.Cover source={{ uri: item.imagem }} />
       <Card.Title title={item.nome} subtitle={item.descricao} />
       <Card.Content>
         <Text style={styles.preco}>{item.preco}</Text>
@@ -40,12 +33,16 @@ export default function App() {
   )
 
   return (
-    <View>
-      <FlatList
-        data={produtos}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-      />
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={produtos}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+      )}
     </View>
   )
 }
@@ -57,7 +54,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#88c5e4'
   },
   card: {
-  
+
     marginBottom: 12,
     borderRadius: 10,
     elevation: 3,
